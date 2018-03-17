fa3fd28145685699f0114a244c41c1bcb6050be2
Richard Cho <richardcho100@gmail.com>
2018-03-17 04:18:17 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,13 @@
 def turn_count(board)
   board.count do |position|
-    position != " "
+    position == "X" || position == "O"
   end
 end
 
 def current_player(board)
   if turn_count(board) % 2 == 0
-    puts "X"
+    "X"
   else
-    puts "O"
+    "O"
   end
 end
