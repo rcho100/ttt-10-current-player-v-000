fa48cfa797203b4868c9134bb6ecf1fb6b93a7b9
Richard Cho <richardcho100@gmail.com>
2018-03-17 04:32:10 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,7 +11,8 @@ def current_player(board)
     "O"
   end
 end
-#if we want to refactor the current_player method, we can use the ternary operator
+
+#if we want to refactor the current_player method, we can use the ternary operator like shown below:
 
 #def current_player(board)
 #  turn_count(board) % 2 == 0 ? "X" : "O"
